ff7995fcb3947801dc653abe3bd15ccdb7ad6e3b
Soso Khurtsidze <sosokhurtsidzee@gmail.com>
2024-01-08 14:31:31 +0400

    add game functionality

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {MathValidators} from "../math-validators";
+import {delay, filter, scan} from "rxjs";
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
   styleUrls: ['./equation.component.scss']
 })
-export class EquationComponent {
+export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl('', [Validators.required])
+  }, [
+    MathValidators.addition('answer', 'a', 'b')
+  ]);
+
+  ngOnInit() {
+    this.mathForm.statusChanges.pipe(
+      filter(value => value === 'VALID'),
+      delay(150),
+      scan((acc) => {
+        return {
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+      }, { numberSolved: 0, startTime: new Date()  })
+    ).subscribe(({ numberSolved, startTime}) => {
+        this.secondsPerSolution = (
+          new Date().getTime() - startTime.getTime()
+        ) / numberSolved / 1000;
+        this.mathForm.setValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: ''
+        })
+
+
+      }
+    )
+  }
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 
 }
